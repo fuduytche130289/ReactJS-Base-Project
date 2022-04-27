a6da92b87a32b8dd5b190746a20a07984ef8b7f4
Cong Duy <rudbich199@gmail.com>
2022-04-27 09:40:00 +0700

    redux

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import Counter from "./pages/Counter";
+
 function App() {
   return (
       <h1 className="text-3xl font-bold underline">
-        Hello world!
+        <Counter/>
       </h1>
   );
 }
---
+++ src/reducers/counterReducer.js
@@ -0,0 +1,15 @@
+//first step
+export default function counterReducer(state = {count: 0}, action) {
+    switch (action.type) {
+        case 'counter/increment':
+            return {
+                count: state.count + action.payload.step
+            };
+        case 'counter/decrement':
+            return {
+                count: state.count - action.payload.step
+            };
+        default:
+            return state;
+    }
+}
