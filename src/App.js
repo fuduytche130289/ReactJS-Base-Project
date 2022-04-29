0d079e99d22ce99b6b1a681930b6d8ff4f78d4ef
Cong Duy <rudbich199@gmail.com>
2022-04-29 14:50:03 +0700

    test admin

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import {BrowserRouter, Router, Routes, Link, Route} from "react-router-dom";
-import List from "./pages/todos/List";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Add from "./pages/todos/Add";
-import Edit from "./pages/todos/Edit";
-import GalleryImage from "./pages/GalleryImage";
-import Login from "./pages/Login";
+
 
 function App() {
-  return (
-      <>
-        <BrowserRouter>
-            <Routes>
-                <Route path = "/" element = {<Add />}/>
-                <Route path = "/add" element = {<Add />}/>
-                <Route path = "/edit" element = {<Edit />}/>
-                <Route path = "/gallery" element = {<GalleryImage />}/>
-                <Route path="/login" element={<Login/>}/>
-            </Routes>
-        </BrowserRouter>
-      </>
-  );
+    return (
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Add/>}/>
+                    <Route path="/add" element={<Add/>}/>
+                </Routes>
+            </BrowserRouter>
+        </>
+    );
 }
 
 export default App;
--- src/pages/todos/Add.js
+++ src/pages/todos/Add.js
@@ -1,14 +1,19 @@
 import React, {useEffect, useState} from 'react';
 import {db} from "../../firebase"
-import {collection, addDoc, doc, getDoc} from "firebase/firestore";
-import {Link, useSearchParams} from "react-router-dom";
+import {collection, addDoc, doc, getDoc, getDocs, query, limit, onSnapshot, deleteDoc} from "firebase/firestore";
+import {Link} from "react-router-dom";
+
 
 const Add = () => {
     const [title, setTitle] = useState('');
     const [content, setContent] = useState('');
-    const [categories, setCategories] = useState([]);
+    const [categories, setCategories] = useState('');
+    const [localCategories, setLocalCategories] = useState([]);
+    const [categoriesId, setCategoriesId] = useState('');
+    const [localPost, setLocalPost] = useState([]);
 
-    const [searchParam] = useSearchParams();
+    let unsub = null;
+
 
     const addPosts = async () => {
         if (!title && !content) {
@@ -19,6 +24,7 @@ const Add = () => {
         await addDoc(collectionRef, {
             title: title,
             content: content,
+            cid: categoriesId
         });
         setTitle('');
         setContent('');
@@ -37,12 +43,53 @@ const Add = () => {
     }
 
     useEffect(() => {
+
         (async () => {
-            const docRef = doc(db, 'categories', searchParam.get('categories'))
-            const docSnapShot = await getDoc(docRef)
-            setCategories(docSnapShot.data().categories)
-        })()
-    }, [])
+            const collectionRef = collection(db, 'categories');
+            unsub = onSnapshot(collectionRef, (snapShot) => {
+                const localCategories = [];
+                snapShot.forEach(doc => {
+                    localCategories.push({
+                        id: doc.id,
+                        categories: doc.data().categories
+                    });
+                });
+                setLocalCategories(localCategories);
+            });
+        })();
+    }, []);
+
+    useEffect(() => {
+
+        (async () => {
+            const collectionRef = collection(db, 'posts');
+            unsub = onSnapshot(collectionRef, (snapShot) => {
+                const localPost = [];
+                snapShot.forEach(doc => {
+                    localPost.push({
+                        id: doc.id,
+                        title: doc.data().title,
+                        content: doc.data().content,
+                        cid: doc.data().cid
+                    });
+                });
+                setLocalPost(localPost);
+            });
+        })();
+    }, []);
+
+    const deleteCategory = async (id) => {
+        const docRef = doc(db, 'categories', id);
+        await deleteDoc(docRef);
+    }
+
+    console.log(localCategories)
+    console.log(localPost)
+
+    const deletePost = async (id) => {
+        const docRef = doc(db, 'posts', id);
+        await deleteDoc(docRef);
+    }
 
     return (
         <div>
@@ -54,7 +101,7 @@ const Add = () => {
                         <div className="flex mt-4 w-full">
                             <input
                                 className="shadow appearance-none border-2 border-teal-500 rounded w-full py-2 px-3 text-grey-darker"
-                                onChange={(evt) => setCategories(evt.target.value)} value={categories}
+                                onChange={(evt) => setCategories(evt.target.value)}
                                 placeholder="Categories"/>
                         </div>
                         <div className="flex mt-4 w-full">
@@ -62,6 +109,17 @@ const Add = () => {
                                 onClick={addCategories}
                                 className="flex-no-shrink p-2 w-full border-2 rounded text-teal border-teal-500 hover:text-teal-500 hover:bg-teal">Add
                             </button>
+                        </div>
+                        <div className="flex flex-col mt-4 w-full">
+                            {localCategories.map((categoriesItem, index) => (
+                                <div key={index} className="flex justify-between">
+                                    <h3 className="flex flex-col justify-center">{categoriesItem.categories}</h3>
+                                    <button
+                                        className="p-2 border-2 rounded text-teal border-teal-500 hover:text-teal-500 hover:bg-teal"
+                                        onClick={() => deleteCategory(categoriesItem.id)}>Delete
+                                    </button>
+                                </div>
+                            ))}
                         </div>
                     </div>
                 </div>
@@ -72,28 +130,31 @@ const Add = () => {
                         <div className="flex mt-4 w-full">
                             <input
                                 className="shadow appearance-none border-2 border-teal-500 rounded w-full py-2 px-3 text-grey-darker"
-                                onChange={(evt) => setTitle(evt.target.value)} value={title}
+                                onChange={(evt) => setTitle(evt.target.value)}
                                 placeholder="Title"/>
                         </div>
                         <div className="flex mt-4 w-full">
                             <input
                                 className="shadow appearance-none border-2 border-teal-500 rounded w-full py-2 px-3 text-grey-darker"
-                                onChange={(evt) => setContent(evt.target.value)} value={content}
+                                onChange={(evt) => setContent(evt.target.value)}
                                 placeholder="Content"/>
                         </div>
 
-                        {categories.map((categories, index) => {
-                                    return (
-                                        <div className="flex mt-4 w-full">
-                                            <select className="border-2 border-teal-500 rounded w-full py-2 px-3 text-grey-darker" key={index}>
-                                                <option value="">{categories}</option>
-                                            </select>
-                                        </div>
-                                    );
-                                })}
 
-
-
+                        <div className="flex mt-4 w-full">
+                            <select onChange={(e) => setCategoriesId(e.target.value)}
+                                    className="border-2 border-teal-500 rounded w-full py-2 px-3 text-teal-500">
+                                {
+                                    localCategories && localCategories?.map((listCategoryItem, listCategoryIndex) => {
+                                        return (
+                                            <option key={listCategoryIndex} value={listCategoryItem.categories}>
+                                                {listCategoryItem.categories}
+                                            </option>
+                                        )
+                                    })
+                                }
+                            </select>
+                        </div>
 
 
                         <div className="flex mt-4 w-full">
@@ -102,8 +163,34 @@ const Add = () => {
                                 className="flex-no-shrink p-2 w-full border-2 rounded text-teal border-teal-500 hover:text-teal-500 hover:bg-teal">Add
                             </button>
                         </div>
-                        <div>{title}</div>
-                        <div>{content}</div>
+                        <div className="flex flex-col">
+                            {
+                                localPost.map((postItem, index) => {
+                                    return (
+                                        <div key={index} className="flex">
+                                            <div className="flex flex-col p-4 m-2">
+                                                <p>Title</p>
+                                                <p className="text-teal-500">{postItem.title}</p>
+                                            </div>
+                                            <div className="flex flex-col p-4 m-2">
+                                                <p> Content</p>
+                                                <p className="text-teal-500">{postItem.content}</p>
+                                            </div>
+                                            <div className="flex flex-col p-4 m-2">
+                                                <p>Category</p>
+                                                <p className="text-teal-500">{postItem.cid}</p>
+                                            </div>
+
+                                            <button
+                                                className="m-2 p-2 border-2 rounded text-teal border-teal-500 hover:text-teal-500 hover:bg-teal"
+                                                onClick={(event => deletePost(postItem.id))}>Delete
+                                            </button>
+
+                                        </div>
+                                    )
+                                })
+                            }
+                        </div>
                     </div>
                 </div>
             </div>
